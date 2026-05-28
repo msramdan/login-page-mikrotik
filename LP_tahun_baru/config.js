29d49b04d9ad0308259cd90d29d2452ccffef0d3
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2026-05-29 03:06:35 +0700

    update

--- LP_tahun_baru/config.js
+++ LP_tahun_baru/config.js
@@ -5,6 +5,6 @@
 (function (w) {
   w.BILLINGHUB_CONFIG = {
     baseUrl: "https://billinghub.id/api",
-    token: "6UUCTOYWf50VJHU9PrUTHtJFamNgtDIl8Jkmg0kwWQ7ph5Xk4xPKAWFhT6VvNtcE",
+    token: "XXXXXXXXXXX",
   };
 })(window);
